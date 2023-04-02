0b052a4cbcc4a3a929ba9b45850749307e6ed1b2
Sinha5714 <shubhamsinha5714@gmail.com>
2023-04-02 19:18:47 +0000

    Add function randomWord which select random word from words and return in uppercase

--- run.py
+++ run.py
@@ -9,5 +9,16 @@ print('||        ||   //          \\  ||     \\ ||  ||      ||    || ||         
 print('||        ||  //            \\ ||      \\||  =========     || ||         || //            \\ ||      \\||')
 print('\n-------------------------------------------------------------------------------------------------------------------------------------------')
 
+ # A list of words from which the random words will be chose for the game
+words = ['flower', 'nurse','house','elephant','jupiter','zebra','human','table','laptop','notebook','kitchen','abroad','accept','bridge','bamboo','escape','excess','guitar','hunter','health','jumble','kitten','legacy','notice']
+
+def randomWord():
+    """
+    Select random word from list words and return in upper case
+    """
+    randomWord = random.choice(words)
+    return randomWord.upper()
+
+
 
 
