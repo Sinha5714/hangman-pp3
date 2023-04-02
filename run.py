2b32371c5135ed01e6bb4452e46d7a328c432af2
Sinha5714 <shubhamsinha5714@gmail.com>
2023-04-02 17:15:43 +0000

    Add list of words used for the game

--- run.py
+++ run.py
@@ -3,3 +3,5 @@ import random
 print('Welcome to Hangman!')
 print('----------------------------------------')
 
+# A list of words from which the random words will be chose for the game
+words = ['flower', 'nurse','house','elephant','jupiter','zebra','human','table','laptop','notebook','kitchen','abroad','accept','bridge','bamboo','escape','excess','guitar','hunter','health','jumble','kitten','legacy','notice']
