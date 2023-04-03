0e16cbe896d361e7f8e31e92e7dd2a414db6bc78
Sinha5714 <shubhamsinha5714@gmail.com>
2023-04-03 13:57:29 +0000

    Add function display_hangman which consist an array of various stages of the game

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import random
 import colorama
 from colorama import Fore
 
-print(Fore.GREEN'\n-------------------------------------------------------------------------------------------------------------------------------------------')
+print('\n-------------------------------------------------------------------------------------------------------------------------------------------\n')
 print(Fore.GREEN +'||        ||        //\\       ||\\      ||  ||============   ||\\      //||       //\\       ||\\      ||')
 print(Fore.RED +'||        ||       //  \\      || \\     ||  ||               || \\    // ||      //  \\      || \\     ||')
 print(Fore.GREEN +'||        ||      //    \\     ||  \\    ||  ||               ||  \\  //  ||     //    \\     ||  \\    ||')
@@ -10,7 +10,7 @@ print(Fore.RED +'||========||     //======\\    ||   \\   ||  ||      ||====|| |
 print(Fore.GREEN +'||        ||    //        \\   ||    \\  ||  ||      ||    || ||         ||   //        \\   ||    \\  ||')
 print(Fore.RED +'||        ||   //          \\  ||     \\ ||  ||      ||    || ||         ||  //          \\  ||     \\ ||')
 print(Fore.GREEN +'||        ||  //            \\ ||      \\||  =========     || ||         || //            \\ ||      \\||')
-print('\n-------------------------------------------------------------------------------------------------------------------------------------------')
+print(Fore.GREEN +'\n-------------------------------------------------------------------------------------------------------------------------------------------')
 
  # A list of words from which the random words will be chose for the game
 words = ['flower', 'nurse','house','packet','rather','zebra','human','table','laptop','rating','notice','abroad','accept','bridge','bamboo','escape','excess','guitar','hunter','health','jumble','kitten','legacy','notice','radius','online']
@@ -22,29 +22,31 @@ def randomWord():
     randomWord = random.choice(words)
     return randomWord.upper()
 
-def enter_name():
-    """
-    Function to enter the player name in alphabets only and return playerName
-    """
-    playerName = input(" Enter your name: " )
-    while True:
-        if playerName.isalpha() and len(playerName) > 3:
-            print(f'Welcome {playerName}')
-        else:
-            print("Invalid Input. Name should only contain alphabets and must be more than three characters")
-        break
+def validate_name():
+    userName = input("Enter your name: \n")
+    if len(userName)>3 and userName.isalpha():
+        print(f"Hello {userName}")
+    
+    else:
+        print("Invalid name. Name must be more than 3 letters and in alphabets")
+        
+    return userName
+       
 def rules():
     """
     Function to display rules of the game
     """
-    playerKnowledge = input("Do you want to see the rules? : y/n ")
-    if playerKnowledge.isalpha():
+    playerKnowledge = input(f"Do you want to see the rules? : YES/NO \n")
+    if playerKnowledge.isalpha() and playerKnowledge.upper() == "YES":
         print("HOW TO PLAY:-")
         print("\nGoal: guess the word and save the man!")
         print("Everytime you can type only one letter.")
         print("Make 6 wrong guesses and you lose. The man will die!")
+    elif playerKnowledge.upper() == "NO":
+        print("Starting Game....")
     else:
         print("Invalid input")
+    return playerKnowledge
     
 
 def play_hangman(randomWord):
@@ -54,11 +56,7 @@ def play_hangman(randomWord):
     guessed_letters = [] # List holds the letters player guessed
     tries = 6  # Total number of tries provided to player
     # Initial display of hangman
-    print("\n+----+")
-    print("|    ")
-    print("|    ")
-    print("|    ")
-    print("===   ")
+    print(display_hangman(tries))
 
     # While loop which will run until the tries are over
     while tries > 0:
@@ -72,10 +70,12 @@ def play_hangman(randomWord):
             # If/Else statement to check the letter was already guessed by user
             if guess in guessed_letters:
                 print(f"You have already guessed the letter {guess}")
+                print(f"\n Attempt left: {tries}")
             elif guess not in randomWord:
                 print("\n You guessed wrong. Try Again")
                 tries -= 1 # Reduce by one with each wrong guess
                 print(f"\n Attempt left: {tries}")
+                print(display_hangman(tries))
                 guessed_letters.append(guess)
             else:
                 print(f"Correct! There is one or more {guess} in the secret word.")         
@@ -93,10 +93,80 @@ def play_hangman(randomWord):
                 print("_", end = ' ')
                 wrong_letter_count += 1 # Increment as the input from user is wrong
         
-        # If wrong_letter_count is 0. Player wins        
+        # If wrong_letter_count is 0. Player wins and the loop breaks        
         if wrong_letter_count == 0:
             print(f"\n Well Done. The secret word was {randomWord}. You saved the man:)")
             break
+    return tries
+
+def display_hangman(tries): 
+    """
+    Function to display various stages of hangman game. It consist of array holding different
+    stage and will be dependent on the tries.
+    """       
+    stages = [  """
+                +----+
+                |    
+                |    
+                |    
+                ===   
+                """,
+                """
+                +----+
+                |    O
+                |    
+                |    
+                ===   
+                """,
+                """
+                +----+
+                |    O
+                |    |
+                |    
+                ===   
+                """,
+                """
+                +----+
+                |    O 
+                |   /| 
+                |     
+                ===    
+                """,
+                """
+                +----+
+                |    O 
+                |   /|\ 
+                |      
+                ===    
+                """,
+                """
+                +----+
+                |    O 
+                |   /|\ 
+                |   / 
+                ===   
+                """,
+                """
+                +----+"
+                |    O 
+                |   /|\ 
+                |   / \ 
+                ===    
+                
+                """
+            ]
+    return stages[tries]
         
+            
+
+answer = "Yes"
+
+while answer == 'Yes':
+    validate_name()
+    rules()
+    play_hangman(randomWord())
+    print("\n Do you want to play again? Yes or No")
+    answer = input()
+    
             
         
