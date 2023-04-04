31b691d56eb4774666a48695806ca3846ec55deb
Sinha5714 <shubhamsinha5714@gmail.com>
2023-04-04 09:11:46 +0000

    Add time intervals in all functions for better visual output

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ print(Fore.RED + "================================================")
 time.sleep(1)
 print(Fore.BLUE + "||   ||   // \\  ||\\  || ||====    ||========   ")
 print(Fore.BLUE + "||===||  //===\\ || \\ || ||  ||=|| ||      | ")
-print(Fore.BLUE + "||   || //     \\||  \\|| ||==|| || ||      |")
+print(Fore.BLUE + "||   || //     \\||  \\|| ||__|| || ||      |")
 time.sleep(1.5)
 print(Fore.BLUE + "==================================||      O ")
 print(Fore.BLUE + "   ||\\  //||  // \\  ||\\  ||       ||     /|\ ")
@@ -47,7 +47,9 @@ def enter_name():
     while True:
         username = input(Fore.CYAN + "\nEnter your name: \n").upper()
         if len(username) > 3:
+            time.sleep(1)
             print(f"{Fore.GREEN}\nWelcome {username} to the Hangman Game:)\n")
+            time.sleep(1.5)
             open_rules()
             break
         else:
@@ -59,13 +61,15 @@ def open_rules():
     """
     # Loop to ask the same question if there is an error
     while True:
-        show_rules = input(Fore.RED+"Open Rules: Y/N \n")
+        show_rules = input(Fore.RED+"Want to open Rules: Y/N \n")
         if show_rules.isalpha() and show_rules.upper() == "Y":
             print("Loading Rules......")
+            time.sleep(2)
             how_to_play()
             break
         elif show_rules.upper() == "N":
             print(f"{Fore.GREEN}\nStarting Game....\n")
+            time.sleep(2)
             break
         else:
             print(f"{Fore.RED}\nInvalid input. Please enter Y or N\n")
@@ -76,10 +80,17 @@ def how_to_play():
     Function to display rules of the game
     """
     print(f"{Fore.CYAN}\nHOW TO PLAY:-")
-    print(f"{Fore.YELLOW}\nGoal: guess the word and save the man!")
+    time.sleep(0.5)
+    print(f"{Fore.YELLOW}Goal: guess the word and save the man!")
+    time.sleep(0.5)
     print(f"{Fore.YELLOW}\nEverytime you can type only one letter.")
+    time.sleep(0.5)
     print(f"{Fore.YELLOW}\nMake 6 wrong guesses and you lose.")
+    time.sleep(0.5)
     print(f"{Fore.YELLOW}\nThe man will die!")
+    time.sleep(4)
+    print(Fore.GREEN + "\nLet's Play!!!!")
+    time.sleep(2)
 
 
 def play_hangman(random_word):
@@ -95,7 +106,8 @@ def play_hangman(random_word):
     tries = 6
 
     # Initial display of hangman game
-    print(Fore.RED + display_hangman(tries))
+    print(Fore.WHITE + display_hangman(tries))
+    print(f"{Fore.RED}\nAttempt left: {tries}\n")
     print(word_length)
 
     # While loop which will run until the tries are over
@@ -110,24 +122,25 @@ def play_hangman(random_word):
         if len(guess) == 1 and guess.isalpha():
             # If/Else statement to check the letter was already guessed by user
             if guess not in random_word:
-                print(Fore.RED + "\n You guessed wrong. Try Again")
+                print(f"{Fore.RED}\n You guessed wrong. Try Again")
                 # Reduce by one with each wrong guess
                 tries -= 1
-                print(Fore.RED + display_hangman(tries))
-                print(f"{Fore.RED}\n Attempt left: {tries}")
+                print(Fore.WHITE + display_hangman(tries))
+                print(f"{Fore.RED}\n Attempt left: {tries}\n")
                 guessed_letters.append(guess)
             elif guess in guessed_letters:
                 print(f"{Fore.YELLOW}\n{guess} is already guessed!")
-                print(display_hangman(tries))
-                print(f"{Fore.RED}\n Attempt left: {tries}")
+                print(Fore.WHITE + display_hangman(tries))
+                print(f"{Fore.RED}\n Attempt left: {tries}\n")
             else:
                 print(f"{Fore.GREEN}\n Correct! {guess} is in the word.")
-                print(f"{Fore.RED}\n Attempt left: {tries}")
-                print(Fore.RED + display_hangman(tries))
+                print(Fore.WHITE + display_hangman(tries))
+                print(f"{Fore.RED}\n Attempt left: {tries}\n")
+                
         else:
-            print(Fore.RED + "\n Invalid input. Enter only one letter")
-            print(Fore.RED + display_hangman(tries))
-            print(f"{Fore.RED}\n Attempt left: {tries}")
+            print(Fore.RED + "\n Invalid input. Enter only one alphabet")
+            print(Fore.WHITE + display_hangman(tries))
+            print(f"{Fore.RED}\n Attempt left: {tries}\n")
     
         # Guess from user added in guessed_letters list
         guessed_letters.append(guess)
@@ -135,17 +148,19 @@ def play_hangman(random_word):
         # For loop to check the value of the input and print it for display
         for letter in random_word:
             if letter in guessed_letters:
-                print(f"{letter}", end=' ')
+                print(f"{Fore.GREEN}{letter}", end=' ')
             else:
-                print(" _ ", end=' ')
+                print(Fore.RED + " _ ", end=' ')
                 # Increment as the input from user is wrong
                 wrong_letter_count += 1
 
         # If wrong_letter_count is 0. Player wins and the loop breaks
         if wrong_letter_count == 0:
-            print(f"\n Well Done. The secret word was {random_word}.\n")
+            print(f"\n\nWell Done. The secret word is {random_word}.\n")
             print("You saved the man:)")
             break
+        elif tries == 0:
+            print(f"\n\n You lose. The secret word was {random_word}.\n")
     return tries
 
 
@@ -213,11 +228,13 @@ def restart_game():
     while True:
         answer = input("\nDo you want to play again?: Y/N\n")
         if answer.upper() == "Y":
-            print("Restarting Game......")
             print("TRY YOUR LUCK AGAIN!!")
+            print("Restarting Game......")
+            time.sleep(2.5)
             enter_name()
             play_hangman(choose_random_word())
         elif answer.upper() == "N":
+            time.sleep(1.5)
             print("Good Bye!!")
             break
         else:
