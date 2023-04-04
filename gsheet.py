260a8e51513305ba7fba8e61f1da8146e3855547
Sinha5714 <shubhamsinha5714@gmail.com>
2023-04-04 16:33:48 +0000

    Add login_data function to exract values from users worksheet

--- gsheet.py
+++ gsheet.py
@@ -11,9 +11,33 @@ CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman_pp3')
+USERS = SHEET.worksheet('users')
+RESULT = SHEET.worksheet('result')
 
-users = SHEET.worksheet('users')
-result = SHEET.worksheet('result')
 
-data = result.get_all_values()
-print(data)
+def login_data():
+    """
+    Gets login data from the player and adds it to the users worksheet
+    """
+    user_login = USERS.get_all_values()
+    return user_login
+
+
+def user_result():
+    """
+    Gets user results and adds to the result worksheet
+    """
+    result = RESULT.get_all_values()
+    return result 
+
+def get_user_details():
+    """
+    To get username for the game
+    """
+    user_login = input("Enter your username:\n")
+    user_password = input("Enter your password:\n")
+    print(user_login)
+    print(user_password)
+
+
+get_user_details()
