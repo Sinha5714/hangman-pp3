595dadfa9b499fbb709c2bbb62bf2d53297ddbcb
Sinha5714 <shubhamsinha5714@gmail.com>
2023-04-06 09:26:36 +0000

    Update print statement in play hangman function

--- run.py
+++ run.py
@@ -212,7 +212,7 @@ def play_hangman(random_word):
             print("\nYou saved the man:)")
             break
         if tries == 0:
-            print(f" You lose. The secret word was {random_word}.\n")
+            print(f"{Fore.GREEN}Missed:( The secret word was {random_word}.\n")
 
     return tries
 
